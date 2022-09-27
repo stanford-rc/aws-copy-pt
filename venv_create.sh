68e449ba19ebb0ab4a02de684f2d985ca17e6834
A. Karl Kornel <akkornel@stanford.edu>
2022-09-27 12:08:05 -0700

    A simple script to quickly create a venv

---
+++ venv_create.sh
@@ -0,0 +1,27 @@
+#/bin/sh
+#-*- coding: utf-8 -*-
+#vim: ts=4 sw=4 noet
+
+# © 2022 The Board of Trustees of the Leland Stanford Junior University
+
+# This script sets up a venv in the project directory.
+
+# Set this to be the specific Python version you want.  If the Python
+# executable is in your $PATH, then you just need the name; otherwise you might
+# need the absolute path.
+PYTHON=python3.10
+
+# Make the venv
+echo "Creating venv with Python ${PYTHON}"
+$PYTHON -m venv .
+
+# Everything after this uses the local venv.
+
+# Upgrade pip, and install wheel
+echo "Installing/Upgrading pip & wheel"
+./bin/python -m pip install --upgrade pip
+./bin/python -m pip install wheel
+
+# Run the setup program, in user-editable (or "dev") mode.
+echo "Running pip install (editable mode)"
+./bin/python -m pip install -e .
